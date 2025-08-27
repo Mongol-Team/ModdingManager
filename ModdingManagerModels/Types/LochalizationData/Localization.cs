9d2c1189e54eeca6f3bc26974ec0a08fc6ed0e1d
Timbuhtuk <timpfoy@gmail.com>
2025-08-27 14:39:00 +0300

    Localization model added

---
+++ ModdingManagerModels/Types/LochalizationData/Localization.cs
@@ -0,0 +1,8 @@
+﻿namespace ModdingManagerModels.Types.LochalizationData
+{
+    public class Localization
+    {
+        public string Name { get; set; }
+        public Dictionary<string, string> Data { get; set; }
+    }
+}
